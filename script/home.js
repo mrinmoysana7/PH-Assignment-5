10faf0718978e169963371dd5c911651f1c7a3e5
mrinmoysana7 <mrinmoysana7@gmail.com>
2026-03-17 00:03:17 +0530

    Issue card done without any function

--- script/home.js
+++ script/home.js
@@ -19,3 +19,15 @@ function switchTab (tab) {
 }
 
 switchTab(currentTab);
+
+const loadIssue = () => {
+    const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues"
+    .then((res) => res.json())
+    .then((data) =>{
+        console.log(data);
+    });
+};
+
+
+
+
