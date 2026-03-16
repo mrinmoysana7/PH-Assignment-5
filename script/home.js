15fd53f04363e12d71e06a3d50c90554c386cfc1
mrinmoysana7 <mrinmoysana7@gmail.com>
2026-03-16 20:05:52 +0530

    Current tab issue solve

---
+++ script/home.js
@@ -0,0 +1,21 @@
+let currentTab = "all";
+const tabActive = ["bg-[#4A00FF]", "text-white", "border-none"];
+const tabInactive = ["bg-white", "text-[#64748B]", "border-[#E4E4E7]" ];
+
+function switchTab (tab) {
+    const allTabs = ["all", "open", "closed"];
+    currentTab = tab;
+    for(const t of allTabs){
+        const tabName = document.getElementById("tab-" + t);
+        if(t === tab){
+            tabName.classList.remove(...tabInactive);
+            tabName.classList.add(...tabActive);
+        } else {
+            tabName.classList.remove(...tabActive);
+            tabName.classList.add(...tabInactive);
+        }
+
+    }
+}
+
+switchTab(currentTab);
